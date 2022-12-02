32329ae2d7f00dfc4c9d9c4980dbdefa8b68b314
marcin10lw <marcin10lw@wp.pl>
2022-12-02 12:40:39 +0100

    created OutputBox component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./Container";
 import Form from "./Form";
 import Exchange from "./Exchange";
 import Result from "./Result";
+import OutputBox from "./OutputBox";
 
 function App() {
   return (
---
+++ src/OutputBox/index.js
@@ -0,0 +1,9 @@
+const OutputBox = () => {
+  return (
+    <p>
+      <div className="form__outputBox"></div>
+    </p>
+  );
+};
+
+export default OutputBox;
