cce65b7f067cdc7e66849862b3cdee0763c63c4d
marcin10lw <marcin10lw@wp.pl>
2022-12-03 14:02:41 +0100

    moved currencies to separate file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import currencies from "./currencies";
 import React, { useState } from "react";
 import Container from "./Container";
 import Form from "./Form";
@@ -5,15 +6,6 @@ import Exchange from "./Exchange";
 import Result from "./Result";
 
 function App() {
-  const currencies = [
-    {id: 1, name: "usd", rate: 4.45},
-    {id: 2, name: "eur", rate: 4.69},
-    {id: 3, name: "gbp", rate: 5.46},
-    {id: 4, name: "nok", rate: 0.46},
-    {id: 5, name: "chf", rate: 4.76},
-    {id: 6, name: "jpy", rate: 0.033},
-    {id: 7, name: "sek", rate: 0.43},
-  ];
   const [currencyIndex, setCurrencyIndex] = useState(0);
   const [amount, setAmount] = useState("");
   const [result, setResult] = useState("N/A");
---
+++ src/currencies.js
@@ -0,0 +1,11 @@
+const currencies = [
+    {id: 1, name: "usd", rate: 4.45},
+    {id: 2, name: "eur", rate: 4.69},
+    {id: 3, name: "gbp", rate: 5.46},
+    {id: 4, name: "nok", rate: 0.46},
+    {id: 5, name: "chf", rate: 4.76},
+    {id: 6, name: "jpy", rate: 0.033},
+    {id: 7, name: "sek", rate: 0.43},
+  ];
+
+  export default currencies;
