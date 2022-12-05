9311f09be3b939dcb1b8d45fea8f3129b19122b5
marcin10lw <marcin10lw@wp.pl>
2022-12-05 13:03:24 +0100

    removed redundant imports from App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import currencies from "./currencies";
 import React, { useState } from "react";
 import Container from "./Container";
 import Form from "./Form";
-import Rate from "./Rate";
-import Result from "./Result";
 
 function App() {
   return (
