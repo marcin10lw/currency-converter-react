2a6bb67a1cd99d6a24acefb1716823d09071a3d6
marcin10lw <marcin10lw@wp.pl>
2022-12-06 22:21:31 +0100

    corrected Clock formatting

--- src/App/Container/Form/Clock/index.js
+++ src/App/Container/Form/Clock/index.js
@@ -1,27 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  
+  useEffect(() => {
+    const dateIntervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
 
-    useEffect(() => {
-        const dateIntervalId = setInterval(() => {
-          setDate(new Date());
-        }, 1000);
-    
-        return () => {
-          clearInterval(dateIntervalId);
-        }
-      }, [date]);
-    
-    const day = date.toLocaleDateString(
-      undefined,
-      {weekday: "long", day: "numeric", month: "long"}
-    );
-    const time = date.toLocaleTimeString();
+    return () => {
+      clearInterval(dateIntervalId);
+    };
+  }, [date]);
 
-    return (
-        <p className="form__date">{`Dzisiaj jest ${day}, ${time}`}</p>
-    );
-}
+  const day = date.toLocaleDateString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+  const time = date.toLocaleTimeString();
+
+  return <p className="form__date">{`Dzisiaj jest ${day}, ${time}`}</p>;
+};
 
 export default Clock;
