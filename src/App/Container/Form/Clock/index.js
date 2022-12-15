70699a9e6724acfc010158605016beca3364e9cc
marcin10lw <marcin10lw@wp.pl>
2022-12-15 19:21:33 +0100

    created useCurrentDate hook

--- src/App/Container/Form/Clock/index.js
+++ src/App/Container/Form/Clock/index.js
@@ -1,26 +1,10 @@
 import "./style.css";
 import React, { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
-  const [date, setDate] = useState(new Date());
+  const [day, time] = useCurrentDate();
   
-  useEffect(() => {
-    const dateIntervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(dateIntervalId);
-    };
-  }, [date]);
-
-  const day = date.toLocaleDateString(undefined, {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  });
-  const time = date.toLocaleTimeString();
-
   return <p className="date">{`Dzisiaj jest ${day}, ${time}`}</p>;
 };
 
---
+++ src/App/Container/Form/Clock/useCurrentDate.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useCurrentDate = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const dateIntervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(dateIntervalId);
+    };
+  }, [date]);
+
+  const day = date.toLocaleDateString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+  const time = date.toLocaleTimeString();
+
+  return [day, time];
+};
+
+export {useCurrentDate};
