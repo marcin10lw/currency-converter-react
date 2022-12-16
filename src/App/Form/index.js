1328c9e40b25e1f0d168b4edde196541b8006c7d
marcin10lw <marcin10lw@wp.pl>
2022-12-16 14:21:06 +0100

    changed Form formatting and removed redundant React

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,46 +1,48 @@
-import React, {useState} from "react";
+import { useState } from "react";
 import Rate from "./Rate";
 import Result from "./Result";
 import Clock from "../Clock";
 import { StyledForm, Legend, Fieldset, Text, Field, Button } from "./styled";
 
-const Form = ({currencies}) => {
+const Form = ({ currencies }) => {
   const [index, setIndex] = useState(0);
   const [amount, setAmount] = useState("");
   const [result, setResult] = useState("N/A");
-  
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentValue, setCurrentValue] = useState("");
 
-  const getIndex = (index) =>{
+  const getIndex = (index) => {
     setIndex(index);
-  }
-  
+  };
+
   const getAmount = (inputValue) => {
     setAmount(inputValue);
-  }
+  };
 
   const getResult = (inputValue, currentRate) => {
     setResult(+inputValue * currentRate);
-  }
-  
-  const onSelectChange = ({target}) => {
-    setCurrentIndex(currencies.findIndex(currency => {
-      return currency.name === target.value.toLowerCase();
-    }))
-  }
+  };
 
-  const onInputChange = ({target}) => {
+  const onSelectChange = ({ target }) => {
+    setCurrentIndex(
+      currencies.findIndex((currency) => {
+        return currency.name === target.value.toLowerCase();
+      })
+    );
+  };
+
+  const onInputChange = ({ target }) => {
     setCurrentValue(target.value);
-  }
+  };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    
+
     getIndex(currentIndex);
     getAmount(currentValue);
     getResult(currentValue, currencies[currentIndex].rate);
-  }
+  };
 
   return (
     <StyledForm onSubmit={onFormSubmit}>
@@ -51,11 +53,10 @@ const Form = ({currencies}) => {
           <label>
             <Text>Wybierz walutę:</Text>
 
-            <Field
-              as="select"
-              onChange={onSelectChange}
-            >
-                {currencies.map(({name, id}) => <option key={id}>{name.toUpperCase()}</option>)}
+            <Field as="select" onChange={onSelectChange}>
+              {currencies.map(({ name, id }) => (
+                <option key={id}>{name.toUpperCase()}</option>
+              ))}
             </Field>
           </label>
         </p>
@@ -73,8 +74,8 @@ const Form = ({currencies}) => {
           </label>
         </p>
         <Rate
-            rate={currencies[index].rate}
-            currency={currencies[index].name.toUpperCase()}
+          rate={currencies[index].rate}
+          currency={currencies[index].name.toUpperCase()}
         />
       </Fieldset>
       <p>
