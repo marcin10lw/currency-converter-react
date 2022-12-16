cceba8099ee0360adbe2d1faeddb925f7554ab44
marcin10lw <marcin10lw@wp.pl>
2022-12-16 14:18:55 +0100

    exported Clock's day and time as object

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -2,7 +2,7 @@ import { useCurrentDate } from "./useCurrentDate";
 import { StyledDate } from "./styled";
 
 const Clock = () => {
-  const [day, time] = useCurrentDate();
+  const {day, time} = useCurrentDate();
   
   return <StyledDate>{`Dzisiaj jest ${day}, ${time}`}</StyledDate>;
 };
--- src/App/Clock/useCurrentDate.js
+++ src/App/Clock/useCurrentDate.js
@@ -20,7 +20,7 @@ const useCurrentDate = () => {
   });
   const time = date.toLocaleTimeString();
 
-  return [day, time];
+  return {day, time};
 };
 
 export {useCurrentDate};
