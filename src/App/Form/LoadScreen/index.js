e6cd15d4cafa0a1102cab55657f999503d2af026
marcin10lw <marcin10lw@wp.pl>
2022-12-30 23:00:24 +0100

    created LoadScreen component

---
+++ src/App/Form/LoadScreen/index.js
@@ -0,0 +1,9 @@
+import { StyledLoadScreen } from "./styled";
+
+const LoadScreen = () => (
+  <StyledLoadScreen>
+    <p>Trwa ładowanie kursów walut z NBP...</p>
+  </StyledLoadScreen>
+);
+
+export default LoadScreen;
---
+++ src/App/Form/LoadScreen/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledLoadScreen = styled.div`
+  text-align: center;
+
+  p{
+    font-size: 30px;
+    font-weight: 700;
+    color: #335d80;
+    margin: 60px 0;
+  }
+`
