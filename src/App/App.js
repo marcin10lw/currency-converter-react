7681aed7f808d6f5a4b16abdbf1b998227366854
marcin10lw <marcin10lw@wp.pl>
2023-02-26 21:59:02 +0100

    added theme and ThemeProvider

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
 import GlobalStyle from "../GlobalStyle";
-import Container from "./Container";
+import { Container } from "./Container/styled";
 import Form from "./Form";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
         <Form />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    grey: "#808080",
+    horizon: "#5a7b96",
+    white: "#ffffff",
+    coffee: "#652626",
+    chambray: "#335d80",
+    tundora: "#493f3f",
+  },
+  breakpoints: {
+    mobile: 767,
+  },
+};
