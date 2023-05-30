df4741c14019f783e91a2e6e5adba7ea39fc22b1
marcin10lw <marcin10lw@wp.pl>
2023-05-30 23:57:02 +0200

    changed index.js to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 const queryClient = new QueryClient();
 
